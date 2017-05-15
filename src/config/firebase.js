76592be5911cd3e1e02da8e65126c91360b829f6
Antonio Calapez <antonio@hackages.io>
2017-05-15 16:23:37 +0200

    Adding firebase config

---
+++ src/config/firebase.js
@@ -0,0 +1,9 @@
+ export const config = {
+  apiKey: "AIzaSyCWmo6Rj40AGwhFbXYr-MP_7sLU0LFjyj4",
+  authDomain: "chat-app-blog.firebaseapp.com",
+  databaseURL: "https://chat-app-blog.firebaseio.com",
+  projectId: "chat-app-blog",
+  storageBucket: "chat-app-blog.appspot.com",
+  messagingSenderId: "130550697398"
+};
+
