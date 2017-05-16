b2f187ed10cf4752083c4f57fd5ed3eb2ee13425
Antonio Calapez <antonio@hackages.io>
2017-05-16 10:21:33 +0200

    Updating App.js and adding some styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
 import {Form} from "./components/form";
+import {MessageList} from "./components/MessageList";
+import {UserProfile} from "./components/UserProfile";
+
+import './App.css';
+
+const updateState = (name, value) => state => ({
+  ...state,
+  [name]: value
+});
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: 'Guest',
+      picture: 'https://hackages.io/static/media/m041074.a59d6a20.jpg',
+      message: ''
+    }
+  }
+
+  update = ({target: {name, value}}) => {
+    this.setState(updateState(name, value));
+  };
+
+  sendMessage = () => {
+
+  };
+
   render() {
     return (
-      <div>
+      <div className="app-container">
+        <UserProfile
+          profile={{...this.state}}
+          update={this.update}/>
         <p>
           Awesome chat!
         </p>
-        <Form />
+        <MessageList />
+        <Form messageValue={this.state.message} update={this.update} sendMessage={this.sendMessage} />
       </div>
     );
   }
