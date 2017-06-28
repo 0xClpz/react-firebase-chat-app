ff11c4f9c80d3a77114fd23c6f16a7e5898d3faa
Antonio Calapez <antonio@hackages.io>
2017-06-28 14:21:04 +0200

    Removing the editing capabilities from the UserProfile.js

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,71 +1,19 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-const toggleKey = key => state => ({
-  ...state,
-  [key]: !state[key]
-});
-
-const toggleEditName = toggleKey('editingName');
-const toggleEditPicture = toggleKey('editingPicture');
-
-export class UserProfile extends Component {
-  state = {
-    editingPicture: false,
-    editingName: false,
-  };
-
-  render(){
-    const {profile} = this.props;
-    return (
-      <div className="user-profile">
-        <div className="profile-picture">
-          <img
-            src={profile.picture}
-            alt="User profile"
-            onDoubleClick={() => this.setState(toggleEditPicture)}/>
-          {this.state.editingPicture &&
-            <span>
-              <input
-                type="text"
-                name="picture"
-                onChange={this.props.update}
-                value={profile.picture} />
-
-              <button
-                onClick={() => this.setState(toggleEditPicture)}>
-                Ok
-              </button>
-            </span>
-          }
-        </div>
-        {
-          this.state.editingName ?
-            <span className="profile-name">
-              <input
-                type="text"
-                name="name"
-                onChange={this.props.update}
-                value={profile.name}/>
-              <button
-                onClick={() => this.setState(toggleEditName)}>
-                Ok
-              </button>
-            </span>
-            :
-            <p
-              onDoubleClick={() => this.setState(toggleEditName)}>
-              {profile.name}
-            </p>
-        }
-      </div>
-    );
-  }
-}
+export const UserProfile = ({profile}) =>
+  <div className="user-profile">
+    <div className="profile-picture">
+      <img
+        src={profile.photoURL}
+        alt="User profile"/>
+    </div>
+    <p>{profile.displayName}</p>
+  </div>;
 
 UserProfile.propTypes = {
   profile: PropTypes.shape({
-    name: PropTypes.string,
-    picture: PropTypes.picture,
+    displayName: PropTypes.string,
+    photoURL: PropTypes.picture,
   }).isRequired,
 };
